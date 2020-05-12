08e101be6af3781c15e818d3d6f9aae4eb77bd3f
tiot07 <tiot7155@gmail.com>
2020-05-12 16:06:56 +0900

    initial commit

---
+++ app/src/main/java/jp/ac/titech/itpro/sdl/helloandroid/MainActivity.kt
@@ -0,0 +1,30 @@
+package jp.ac.titech.itpro.sdl.helloandroid
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+
+const val EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE"
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    /** Called when the user taps the Send button */
+    fun sendMessage(view: View) {
+        val editText = findViewById<EditText>(R.id.editText)
+        val message = editText.text.toString()
+//        val intent = Intent(this, DisplayMessageActivity::class.java).apply {
+//            putExtra(EXTRA_MESSAGE, message)
+//        }
+
+//        startActivity(intent)
+        val textView = findViewById<TextView>(R.id.textView).apply {
+            text = "Hello " + message + "!\nNice to see you!"
+        }
+    }
+}
